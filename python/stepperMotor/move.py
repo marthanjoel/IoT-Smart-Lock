3a876becd5df5716701b44bfc21e713e81abc60e
nexoe <rasmusnexoe@gmail.com>
2017-03-15 13:19:54 +0100

    reduced move.py

--- python/stepperMotor/move.py
+++ python/stepperMotor/move.py
@@ -2,7 +2,7 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 class Motor(object):
-    def __init__(self, pins, mode=3):
+    def __init__(self, pins):
         """Initialise the motor object.
 
         pins -- a list of 4 integers referring to the GPIO pins that the IN1, IN2
@@ -17,7 +17,6 @@ class Motor(object):
         self.P2 = pins[1]
         self.P3 = pins[2]
         self.P4 = pins[3]
-        self.mode = mode
         self.deg_per_step = 5.625 / 64  # for half-step drive (mode 3)
         self.steps_per_rev = int(360 / self.deg_per_step)  # 4096
         self.step_angle = 0  # Assume the way it is pointing is zero degrees
@@ -44,16 +43,10 @@ class Motor(object):
         if steps > self.steps_per_rev / 2:
             steps -= self.steps_per_rev
             print "moving " + `steps` + " steps"
-            if self.mode == 2:
-                self._move_acw_2(-steps / 8)
-            else:
-                self._move_acw_3(-steps / 8)
+            self._move_acw_2(-steps / 8)
         else:
             print "moving " + `steps` + " steps"
-            if self.mode == 2:
-                self._move_cw_2(steps / 8)
-            else:
-                self._move_cw_3(steps / 8)
+            self._move_cw_2(steps / 8)
         self.step_angle = target_step_angle
 
     def __clear(self):
@@ -61,7 +54,7 @@ class Motor(object):
         GPIO.output(self.P2, 0)
         GPIO.output(self.P3, 0)
         GPIO.output(self.P4, 0)
-
+#Counter clockwise
     def _move_acw_2(self, big_steps):
         self.__clear()
         for i in range(big_steps):
@@ -77,7 +70,7 @@ class Motor(object):
             GPIO.output(self.P4, 0)
             GPIO.output(self.P2, 1)
             sleep(self._T * 2)
-
+#Clockwise
     def _move_cw_2(self, big_steps):
         self.__clear()
         for i in range(big_steps):
@@ -94,45 +87,11 @@ class Motor(object):
             GPIO.output(self.P1, 1)
             sleep(self._T * 2)
 
-    def _move_acw_3(self, big_steps):
-        self.__clear()
-        for i in range(big_steps):
-            GPIO.output(self.P1, 0)
-            sleep(self._T)
-            GPIO.output(self.P3, 1)
-            sleep(self._T)
-            GPIO.output(self.P4, 0)
-            sleep(self._T)
-            GPIO.output(self.P2, 1)
-            sleep(self._T)
-            GPIO.output(self.P3, 0)
-            sleep(self._T)
-            GPIO.output(self.P1, 1)
-            sleep(self._T)
-            GPIO.output(self.P2, 0)
-            sleep(self._T)
-            GPIO.output(self.P4, 1)
-            sleep(self._T)
+    def unlock(self):
+        self.move_to(90)
 
-    def _move_cw_3(self, big_steps):
-        self.__clear()
-        for i in range(big_steps):
-            GPIO.output(self.P3, 0)
-            sleep(self._T)
-            GPIO.output(self.P1, 1)
-            sleep(self._T)
-            GPIO.output(self.P4, 0)
-            sleep(self._T)
-            GPIO.output(self.P2, 1)
-            sleep(self._T)
-            GPIO.output(self.P1, 0)
-            sleep(self._T)
-            GPIO.output(self.P3, 1)
-            sleep(self._T)
-            GPIO.output(self.P2, 0)
-            sleep(self._T)
-            GPIO.output(self.P4, 1)
-            sleep(self._T)
+    def lock(self):
+        self.move_to(0)
 
 
 if __name__ == "__main__":
@@ -140,12 +99,9 @@ if __name__ == "__main__":
     m = Motor([18,22,24,26])
     m.rpm = 5
     print "Pause in seconds: " + `m._T`
-    m.move_to(90)
-    sleep(1)
-    m.move_to(0)
-    sleep(1)
-    m.mode = 2
-    m.move_to(90)
-    sleep(1)
-    m.move_to(0)
+    while(True):
+        m.unlock()
+        sleep(1)
+        m.lock()
+        sleep(1)
     GPIO.cleanup()
